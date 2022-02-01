b6179b3bd594ba7fa62117738fc0c36ad305f828
floydkim <for12gm@gmail.com>
2022-02-01 18:28:39 +0900

    select more elements

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
 const $view = document.getElementById("carousel");
 
 console.log("$view", $view);
+
+const $container = $view?.querySelector(".container");
+
+console.log("$container", $container);
+
+const PANEL_COUNT = $container?.querySelectorAll(".panel").length;
+
+console.log("PANEL_COUNT", PANEL_COUNT);
