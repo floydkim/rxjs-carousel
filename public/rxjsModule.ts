5a2569e8cffac3854a3874446a0f37931e7b9eaa
floydkim <for12gm@gmail.com>
2022-02-04 13:24:44 +0900

    drag$는 드래그한 거리를 값으로 emit한다

--- public/rxjsModule.ts
+++ public/rxjsModule.ts
@@ -1,4 +1,4 @@
-import {fromEvent, map, merge, mergeAll, mergeMap, switchMap, takeUntil, tap} from "rxjs";
+import {fromEvent, map, merge, mergeAll, mergeMap, pluck, switchMap, takeUntil, tap} from "rxjs";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const $view = document.getElementById("carousel")!; // null이 아님을 보장할 수 있음
@@ -57,9 +57,25 @@ export const makeObservable = () => {
      * 책에서는 unhappy한 케이스를 대비하기 위해서일지, 일반적으로 적용해도 괜찮은 오퍼레이터여서 그런건지
      * `switchMap`을 사용하는 것이 최종 코드임.
      * (책설명: start$에서 데이터가 발생할 때마다 move$이 생성되기 때문에 기존 데이터를 자동으로 종료하기 위해서)
+     * ```
+        const drag$ = start$.pipe(
+            switchMap(start => move$.pipe(takeUntil(end$))),
+        );
+     * ```
+     * (흔한 unhappy case : 드래그 중 view$ 범위를 벗어나면 end$가 값 발생을 안하므로 move$가 종료되지 않음)
+     *
+     *
+     * 다음으로, 캐러셀을 움직이려면 클릭한 시점부터 x 좌표로 얼만큼 드래그했는지 알아야 함.
+     * 기준 좌표는 start$안의 이벤트로부터 pageX 속성을 통해 알아내고, move$로부터 마우스가 얼만큼 움직였는지 알아낸다.
+     * 결국 drag$는 드래그한 x 거리를 emit 하는 옵저버블이 된다.
      */
     const drag$ = start$.pipe(
-        switchMap(start => move$.pipe(takeUntil(end$))),
+        switchMap(start => {
+            return move$.pipe(
+                map(move => move.pageX - start.pageX),
+                takeUntil(end$),
+            );
+        }),
     );
 
     drag$.subscribe((e) => console.log("e", e));
